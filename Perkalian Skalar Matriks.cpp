a705be14e3e383b9e282130ee8f3dc1c00b68e14
RevinaAurigha03 <90344666+RevinaAurigha03@users.noreply.github.com>
2022-09-27 10:22:23 +0700

    Add files via upload

---
+++ Perkalian Skalar Matriks.cpp
@@ -0,0 +1,35 @@
+// Revina Aurigha Firdaus 21091397003
+// Tugas perkalian skalar matrik
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	int i, j, m, n, skalar, matriksA[10][10], hasil[10][10];
+  
+	cout << " PROGRAM PERKALIAN SKALAR MATRIKS " << endl;
+	cout << "___________________________________" << endl;
+	cout << " Jumlah baris matriks : "; cin >> m;
+	cout << " Jumlah kolom matriks : "; cin >> n;
+	
+	cout << endl;
+	cout << " Masukkan Nilai Pengali : "; cin >> skalar;
+  	cout << " Matrix A : \n";
+	for(i=0; i<m; i++){
+		for(j=0; j<n; j++){
+      		cin>>matriksA[i][j];
+    	}
+  	}
+  	cout<<" Hasil pengurangan matrix : \n";
+  	for(i=0; i<m; i++){
+    	for(j=0; j<n; j++){
+      		hasil[i][j]=skalar*matriksA[i][j];
+      		cout<<hasil[i][j]<<"\t";
+    	}
+    cout<<endl;
+	}
+  
+	getch();
+	return 0;
+}
